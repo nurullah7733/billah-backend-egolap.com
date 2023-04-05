d554adc36c8b72f7a05493a8639b96ec983560dc
nurullah7733 <nurullah7733@gmail.com>
2023-04-06 01:47:25 +0600

    imgs2

--- src/middlewares/uploadImgMiddleware.js
+++ src/middlewares/uploadImgMiddleware.js
@@ -1,7 +1,7 @@
 const multer = require("multer");
 const sharp = require("sharp");
 const path = require("path");
-const fs = require("/");
+const fs = require("fs");
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
