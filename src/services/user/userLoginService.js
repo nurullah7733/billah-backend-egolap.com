eefafe573022e062f307bb04d25769f5ecb2ca70
nurullah <nurullah7733@gmail.com>
2023-10-16 22:42:05 +0600

    cookie time set 1month

--- src/services/user/userLoginService.js
+++ src/services/user/userLoginService.js
@@ -23,12 +23,13 @@ const userLoginService = async (Request, Response, DataModel) => {
         }
 
         Response.cookie("token", token, {
-          maxAge: 1000 * 60 * 60 * 24 * 60,
+          maxAge: 1000 * 60 * 60 * 24 * 30, // 1 month
           httpOnly: true,
           sameSite: "lax",
           path: "/",
           secure: process.env.NODE_ENV === "production",
-        }); 
+        });
+
         return {
           status: "success",
           data: {
