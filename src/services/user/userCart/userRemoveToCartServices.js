6a867d6b1b97a488b8d9e90a61aa71dceefbac92
nurullah <nurullah7733@gmail.com>
2023-10-20 22:50:45 +0600

    userModel Update Cart item

--- src/services/user/userCart/userRemoveToCartServices.js
+++ src/services/user/userCart/userRemoveToCartServices.js
@@ -1,14 +1,20 @@
+const { default: mongoose } = require("mongoose");
+
 const userRemoveToCartService = async (Request, DataModel) => {
   let id = Request.params.id;
   let reqBody = Request.body;
   let productId = reqBody.productId;
 
   try {
-    let data = await DataModel.updateOne(
-      { _id: id },
-      { $pull: { cart: { _id: productId } } }
-    );
-    return { status: "success", data };
+    if (mongoose.Types.ObjectId.isValid(id)) {
+      let data = await DataModel.updateOne(
+        { _id: id },
+        { $pull: { cart: { _id: productId } } }
+      );
+      return { status: "success", data };
+    } else {
+      return { status: "fail", data: "User id invalid" };
+    }
   } catch (error) {
     return { status: "fail", data: error };
   }
