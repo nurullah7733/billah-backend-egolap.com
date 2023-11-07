658bd1c7fe0ae341f6c605681733279a6c4911e6
nurullah <nurullah7733@gmail.com>
2023-11-07 22:34:17 +0600

    cookie domain set

--- src/services/user/userLoginService.js
+++ src/services/user/userLoginService.js
@@ -17,7 +17,7 @@ const userLoginService = async (Request, Response, DataModel) => {
         }
 
         Response.cookie("token", token, {
-          domain: "http://localhost:3000/",
+          domain: "https://e-golap.vercel.app/",
           maxAge: 1000 * 60 * 60 * 24 * 30, // 1 month
           httpOnly: true,
           sameSite: "none",
