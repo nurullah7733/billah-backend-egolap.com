9a08d2eeaedeeef81d7e6e30fd15a96419abd432
nurullah <nurullah7733@gmail.com>
2023-11-07 22:16:11 +0600

    cookie domain add

--- src/services/user/userLoginService.js
+++ src/services/user/userLoginService.js
@@ -17,7 +17,7 @@ const userLoginService = async (Request, Response, DataModel) => {
         }
 
         Response.cookie("token", token, {
-          // domain: process.env.COOKIES_DOMAIN,
+          domain: "http://localhost:3000/",
           maxAge: 1000 * 60 * 60 * 24 * 30, // 1 month
           httpOnly: true,
           sameSite: "none",
