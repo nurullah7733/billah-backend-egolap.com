a6dce260a26ea93a29dcea7221c3064b54ee247f
nurullah <nurullah7733@gmail.com>
2023-11-07 17:05:53 +0600

    cookie update

--- src/services/user/userLoginService.js
+++ src/services/user/userLoginService.js
@@ -17,10 +17,10 @@ const userLoginService = async (Request, Response, DataModel) => {
         }
 
         Response.cookie("token", token, {
-          domain: "http://localhost:3000",
+          // domain: "http://localhost:3000",
           maxAge: 1000 * 60 * 60 * 24 * 30, // 1 month
           httpOnly: true,
-          sameSite: "lax",
+          // sameSite: "lax",
           path: "/",
           secure: process.env.NODE_ENV === "production",
         });
