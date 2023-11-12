4f4ef5bc52fc6f0f6fe8c81f29fdde916e4be4f1
nurullah <nurullah7733@gmail.com>
2023-11-12 10:19:52 +0600

    trust proxy

--- app.js
+++ app.js
@@ -32,7 +32,7 @@ app.use(
   })
 );
 // app.set("trust proxy", 1);
-// app.enable("trust proxy");
+app.enable("trust proxy");
 app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Credentials", true);
   res.header("Access-Control-Allow-Origin", req.headers.origin);
