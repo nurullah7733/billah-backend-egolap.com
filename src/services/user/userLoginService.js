c4989417051a7daa0b60ffb53782408178a65f31
nurullah <nurullah7733@gmail.com>
2023-11-12 15:09:49 +0600

    cookie domain update

--- src/services/user/userLoginService.js
+++ src/services/user/userLoginService.js
@@ -17,7 +17,7 @@ const userLoginService = async (Request, Response, DataModel) => {
         }
 
         Response.cookie("token", token, {
-          domain: "https://e-golap-server.up.railway.app/",
+          domain: [".railway.app/", ".verce.app"],
           // maxAge: 1000 * 60 * 60 * 24 * 30, // 1 month
           maxAge: 2592000, // 1 month
           httpOnly: process.env.NODE_ENV === "production",
@@ -25,7 +25,6 @@ const userLoginService = async (Request, Response, DataModel) => {
           path: "/",
           secure: process.env.NODE_ENV === "production",
         });
-        console.log(process.env.NODE_ENV === "production", "pprocess");
 
         return {
           status: "success",
